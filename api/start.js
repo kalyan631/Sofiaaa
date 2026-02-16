c1397a1091547c60c7c7f75f8e866079edc01e70
🖤ः⎯꯭̽࠾࡙ࠬ𝆬иσв࡙ιтα𝆺𝅥┽•࡙ꭙ𝆬𝆬𝆬𝆬𝂇࠾ࠬ❤ <kalyanpari67@gmail.com>
2026-02-16 19:27:28 +0530

    Create start.js

---
+++ api/start.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    photo: "https://files.catbox.moe/jl38pt.jpg",
+    caption:
+      "“ Hi 👋 Main Sofia hoon 🤍 ”\n\n" +
+      "━━━━━━━━━━━━━━━\n" +
+      "🖤 BLACK AI CHATBOT\n" +
+      "━━━━━━━━━━━━━━━\n\n" +
+      "❝ Mujhe Black Server par develop kiya gaya hai  \n" +
+      "Aur mujhe @Revenge_mode ne banaya hai 🔥 ❞\n\n" +
+      "━━━━━━━━━━━━━━━\n" +
+      "🤖 Main kya kar sakti hoon?\n" +
+      "━━━━━━━━━━━━━━━\n\n" +
+      "➤ Aapke sawaalon ke jawab  \n" +
+      "➤ AI style smart replies  \n" +
+      "➤ Ideas, help aur guidance  \n\n" +
+      "━━━━━━━━━━━━━━━\n" +
+      "✨ Bas apna sawal bhejo  \n" +
+      "Sofia aapki madad ke liye ready hai 🚀\n" +
+      "━━━━━━━━━━━━━━━"
+  });
+}
